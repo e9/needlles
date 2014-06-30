e3e313bfd4f4aa5363a11824c8a2cac31c43cd66
Shotaro Osaki <t05181so@gmail.com>
2014-07-01 01:21:33 +0900

    cache timezone

--- js/tw.js
+++ js/tw.js
@@ -1,7 +1,13 @@
 var get_timezone = function(screen_name){
+    var d = new $.Deferred;
+
+    var timezone = localStorage[screen_name];
+
+    if (timezone) return d.resolve(timezone);
+
     OAuth.initialize("iwFvDIDlAlxsctdx41ooP1p2jvI");
     OAuth.popup("twitter", function(error, result) {
-        if (error) return;
+        if (error) return d.reject();
         // console.log(error, result)
         // console.log("oauth_token: " + result.oauth_token);
         // console.log("oauth_token_secret: " + result.oauth_token_secret);
@@ -9,15 +15,21 @@ var get_timezone = function(screen_name){
         //     alert('Hello ' + data.screen_name);
         // });
         result.get('/1.1/users/show.json?screen_name=' + screen_name).done(function(data){
-            alert(data.time_zone);
+            var timezone = data.time_zone;
+            localStorage[screen_name] = timezone;
+            d.resolve(timezone);
         });
     });
+
+    return d.promise();
 }
 
 
 $(function(){
     $('#button').click(function(){
         var screen_name = $('#twid').val();
-        get_timezone(screen_name);
+        get_timezone(screen_name).done(function(timezone){
+            alert(timezone);
+        });
     });
 });
